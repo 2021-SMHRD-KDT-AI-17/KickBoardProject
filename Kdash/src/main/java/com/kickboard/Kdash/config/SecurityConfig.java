a86d3f0d1123e3a67135152be48fc7ae744f9714
Valis49 <khw2030@gmail.com>
2024-04-01 16:27:42 +0900

    풀 전 커밋

--- Kdash/src/main/java/com/kickboard/Kdash/config/SecurityConfig.java
+++ Kdash/src/main/java/com/kickboard/Kdash/config/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
@@ -33,4 +35,5 @@ public class SecurityConfig{
 	        
 	    return http.build();
 	}
+	
 }
