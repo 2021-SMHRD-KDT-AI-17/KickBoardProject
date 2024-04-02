806eece13026a99f9b7d5000301021ae5e70ae78
유승주 <yuwe58@naver.com>
2024-04-02 15:44:53 +0900

    비번변경

--- Kdash/src/main/java/com/kickboard/Kdash/controller/ProfileController.java
+++ Kdash/src/main/java/com/kickboard/Kdash/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.kickboard.Kdash.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpSession;
@@ -10,13 +11,16 @@ import com.kickboard.Kdash.mapper.ProfileMapper;
 
 @Controller
 public class ProfileController {
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
 	public ProfileMapper profileMapper;
 	
 	@RequestMapping("/profile_update")
 	public String profile_update(SignupDto sign, HttpSession session) {
-		
+		String encPassword = bCryptPasswordEncoder.encode(sign.getPassword());
+		sign.setPassword(encPassword);
 		profileMapper.profile_update(sign);
 		return"Home";
 	}
